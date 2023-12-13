c551a89f90eb3af13a2583e77905b2d2dbc90e70
shuoshuo <hsieh910075@gmail.com>
2023-12-13 20:32:31 +0800

    Initial Commit

---
+++ 5555/_555App.swift
@@ -0,0 +1,17 @@
+//
+//  _555App.swift
+//  5555
+//
+//  Created by 訪客使用者 on 2023/12/13.
+//
+
+import SwiftUI
+
+@main
+struct _555App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
